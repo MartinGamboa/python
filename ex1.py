2be99bd2f3ecc723c9afa0146b35b5ef5af43c59
MartinGamboa <mgamboa@fipasoft.com.mx>
2016-08-18 11:28:35 -0600

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,2 +1,11 @@
 #Exersise 1
+# -*- coding: utf-8 -*-
+#print "Hello World"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you 'not'."
+print 'I "said" do not thouch this.'
+print "Hello World Hello Again I like typing this. This is fun. 'Yay! Printing'. I'd much rather you 'not' I 'said' do not thouch this."
 
