d058413a14391b45e353bd12d5e7be1b83ddaeaf
MartinGamboa <mgamboa@fipasoft.com.mx>
2016-08-23 15:06:00 -0600

    Add files via upload

---
+++ ex9.py
@@ -0,0 +1,14 @@
+#Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\n"
+
+print "Here are days:", days
+print "Here are the month:",months
+
+print """
+There's somethings goin on here.
+Whith the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5,or 6.
+"""
