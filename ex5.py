f8751d89875cbdc3fe2527253b87e0cfd0042b79
MartinGamboa <mgamboa@fipasoft.com.mx>
2016-08-23 07:03:22 -0600

    Create ex5.py

---
+++ ex5.py
@@ -0,0 +1,19 @@
+name = "Martin Gamboa"
+agen = 32 #is true
+height = 74 #inches
+weight = 180 #lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+height_cm = height*2.74
+weight_kg = weight*.453592
+
+print "Let's talk about %s."% name
+print "He's %d inches tall."%height
+print "He's %d pounds heavy."%weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair."%(eyes,hair)
+print "His teeth are usually %s depending on the coffe."%teeth
+print "Inches tall in cm %d."%height_cm
+print "Kg is %d."%weight_kg
