0fc1ad4e99d2dcd8485fa166284b9caf7d25ecae
MartinGamboa <mgamboa@fipasoft.com.mx>
2016-08-26 13:01:23 -0600

    Add files via upload

---
+++ ex32.py
@@ -0,0 +1,41 @@
+the_count = [1,2,3,4,5]
+fruits =['apples','oranges','pears','apricots']
+change = [1,'pennies',2,'dimes',3,'quarters']
+
+#this first kind of for-loop goes through a list
+
+for number in the_count:
+	print "This is count %d" % number
+
+#same as above
+
+for fruit in fruits:
+	print "A fruit of type: %s" % fruit
+
+#also we can go through mixed lists too
+#notice we have tu user %r since we don't know what's in it
+
+for i in change:
+	print "I got %r" % i
+
+#we can also build lists, first start with an empty one
+elements=[]
+
+#then use the range function to do 0 to 5 counts
+for i in range(0,6):
+	print "Adding %d to the list." % i
+	#append is a function that list understand
+	elements.append(i)
+#now we can print them out too
+for i in elements:
+	print "Element was %d" %i
+
+arrayNew = []
+
+for i in range(0,100):
+	value = i*5;
+	print "Valor asigned is %d" %value
+	arrayNew.append(value)
+
+for valueNew in arrayNew:
+	print "Value is %d" % valueNew 
